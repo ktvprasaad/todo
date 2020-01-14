218cf4e37ae1bc88e7af4f1789304955037d621a
ktvprasaad <47123069+ktvprasaad@users.noreply.github.com>
2020-01-14 01:26:39 -0500

    Updated with Material Design
    Updated with Material Design

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="container">
+    <div class="todoBox">
     <md-field>
-      <md-input v-model="currentTodo" @keydown.enter="addTodo()"
-        maxlength="50" placeholder="Add a Todo"></md-input>
+      <md-input class="md-focussed" v-model="currentTodo" @keydown.enter="addTodo(todo)"
+        placeholder="Key-in Todo Tasks"></md-input>
+      <md-button class="md-primary" @click="addTodo(todo)">
+        Add
+      </md-button>
     </md-field>
+    </div>
+
     <ul class="todos">
       <li v-for="todo in todos" :key="todo.id">
-        <input class="checked" type="checkbox" v-model="todo.completed">
-        <span :class="{completed: todo.completed}" @dblclick="editTodo(todo)" v-if="!todo.edit">
+        <md-checkbox class="md-primary" type="checkbox" v-model="todo.completed">+</md-checkbox>
+        <!-- <input> -->
+        <!-- <md-checkbox v-model="todo.completed"> -->
+        <!-- <input md-checked v-model="todo.completed"> -->
+         <span :class="{completed: todo.completed}" @dblclick="editTodo(todo)" v-if="!todo.edit">
           {{ todo.label }}
-        </span>
+          </span>
         <input type="text" v-else v-model="todo.label" @keydown.enter="editDone(todo)">
-        <md-button @click="removeTodo(todo)">X</md-button>
+        <md-button @click="removeTodo(todo)">
+          <img src="./assets/del.png" alt="delete task">
+        </md-button>
       </li>
     </ul>
   </div>
@@ -19,6 +30,7 @@
 
 <script>
 export default {
+  name: 'RegularCheckboxes',
   data() {
     return {
       todos: [],
@@ -27,12 +39,14 @@ export default {
   },
   methods: {
     addTodo() {
-      this.todos.push({
-        id: this.todos.length, 
-        label: this.currentTodo, 
-        completed: false, 
-        edit: false
-      });
+      if (this.currentTodo.length > 0) {
+        this.todos.push({
+          id: this.todos.length, 
+          label: this.currentTodo, 
+          completed: false, 
+          edit: false
+        });
+      }
       this.currentTodo = '';
     },
     removeTodo(todo) {
@@ -52,26 +66,24 @@ export default {
 <style>
   .container {
     max-width: 300px;
+    margin: auto;
     font-family: 'Roboto', sans-serif;
-    font-size: 40px;
+    font-size: 24px;
     display: inline;
-    color: blue;
   }
-  md-input {
-    max-width: 200px;
-    border-radius: 2px;
-    border-color: blue;
+  .todoBox {
+    margin: 20px;
   }
   li {
     list-style: none;
-    margin: 10px;
+    margin-bottom: 7px;
   }
   .completed {
     text-decoration: line-through;
     color:coral;
   }
-  md-button {
-    font-size: 20px;
-    /* float:right; */
+  .md-checkbox {
+    /* display: flex; */
+    align-items: center;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { MdField } from 'vue-material/dist/components';
+import { MdField, MdButton, MdCheckbox } from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
 
 Vue.use(MdField);
+Vue.use(MdButton);
+Vue.use(MdCheckbox);
 
 Vue.config.productionTip = false
 
