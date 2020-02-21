8cc91e5dd243fe7873eb0e8902123b0d040128e1
ktvprasaad <47123069+ktvprasaad@users.noreply.github.com>
2020-02-21 12:47:40 -0500

    Update App.vue
    Cosmetic changes.

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <div class="todoBox">
     <md-field>
       <md-input class="md-focussed" v-model="currentTodo" @keydown.enter="addTodo(todo)"
-        placeholder="Key-in Todo Tasks"></md-input>
-      <md-button class="md-primary" @click="addTodo(todo)">
+      placeholder="Key-in tasks!" maxlength="50"></md-input>
+      <md-button id="add" class="md-primary" @click="addTodo(todo)">
         Add
       </md-button>
     </md-field>
@@ -12,12 +12,12 @@
 
     <ul class="todos">
       <li v-for="todo in todos" :key="todo.id">
-        <md-checkbox class="md-primary" type="checkbox" v-model="todo.completed">+</md-checkbox>
+        <md-checkbox type="checkbox" title="done" class="md-primary" v-model="todo.completed"></md-checkbox>
          <span :class="{completed: todo.completed}" @dblclick="editTodo(todo)" v-if="!todo.edit">
           {{ todo.label }}
           </span>
-        <input type="text" v-else v-model="todo.label" @keydown.enter="editDone(todo)">
-        <md-button @click="removeTodo(todo)">
+        <md-input type="text" v-else v-model="todo.label" @keydown.enter="editDone(todo)"></md-input>
+        <md-button id="del" title="delete" @click="removeTodo(todo)">
           <img src="./assets/del.png" alt="delete task">
         </md-button>
       </li>
@@ -63,13 +63,13 @@ export default {
 <style>
   .container {
     max-width: 300px;
-    margin: auto;
     font-family: 'Roboto', sans-serif;
     font-size: 24px;
     display: inline;
   }
   .todoBox {
     margin: 20px;
+    display: block;
   }
   li {
     list-style: none;
@@ -77,10 +77,33 @@ export default {
   }
   .completed {
     text-decoration: line-through;
-    color:coral;
+    color:#C3E86C;
+  }
+  /* .md-checkbox { */
+    /* display: flex; */
+    /* align-items: center;
+  } */
+  .md-input {
+    border-bottom: 2px solid #5321DD !important;
+    max-width: 360px;
+    margin: 0 auto;
   }
   .md-checkbox {
-    /* display: flex; */
-    align-items: center;
+    background-color: #C3E86C;
+    display: flex;
+  }
+  /* .md-button:hover { */
+    /* border-radius: 90%; */
+    /* background-color: #C3E86C !important;
+  } */
+  #add, #add:hover {
+    background-color: #5321DD !important;
+    color: white;
+  }
+  #del:hover {
+    background-color: #C3E86C;
+  }
+  .todos {
+    color: #5321DD;
   }
 </style>
